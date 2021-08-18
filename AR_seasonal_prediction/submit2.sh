91d4c04a4d1660a13de7364ab88597206b594ca7
Kai-Chih Tseng <ktseng@cake.fios-router.home>
2021-08-18 10:35:27 -0400

    Your message about the commit

---
+++ AR_seasonal_prediction/submit2.sh
@@ -0,0 +1,6 @@
+git init
+git add 
+git commit -m "Your message about the commit"
+git remote add origin https://github.com/yourUsername/yourRepository.git
+git push -u origin master
+git push origin master
